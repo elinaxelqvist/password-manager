27f6af2ea82bbbc57a682a792164f015d2347d22
elinaxelqvist <118172900+elinaxelqvist@users.noreply.github.com>
2024-02-21 14:57:18 +0100

    Create solution and project files.

---
+++ password-manager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace passwordmanager
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
