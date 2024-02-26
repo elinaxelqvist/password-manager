d9e991cb46c5e38d33fc06893d9ad38f1771477f
elinaxelqvist <118172900+elinaxelqvist@users.noreply.github.com>
2024-02-26 17:56:02 +0100

    Test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        Console.WriteLine(" Hej , " + name + "!");
+    }
+}
