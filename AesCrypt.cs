4f0caaa295ed8f8ac2ddc4024aea3bc3801fa718
Josefine <josefine@systementor.se>
2024-02-28 11:53:21 +0100

    ny fil

---
+++ AesCrypt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.Json;
+
+
+namespace password_manager
+{
+    public class Aes_Kryptering
+    {
+
+
+        public static byte[] GenerateRandomIV()
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.GenerateIV();
+                return aes.IV;
+            }
+        }
+
+        public static void PrintByteArray(byte[] bytes)
+        {
+            foreach (byte b in bytes)
+            {
+                Console.Write(b.ToString("x2") + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+        //test 
+    }
+}
